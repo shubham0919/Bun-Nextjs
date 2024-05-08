1090edd97d737f53d8652a66535fcd4e6002db31
Shubham Sonwane <sonwaneshubham049@gmail.com>
2024-05-09 03:10:49 +0530

    deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://bun-hono.sonwaneshubham049.workers.dev",
         changeOrigin: true,
       },
     },
